340f3d68bbe202f9a4d71117d11d01277c4bfcc9
Andrey Pahomov <pahomov.and@gmail.com>
2026-02-08 10:40:20 +0200

    fix
    Signed-off-by: Andrey Pahomov <pahomov.and@gmail.com>

--- src/input_modifiers.c
+++ src/input_modifiers.c
@@ -214,7 +214,7 @@ static int sym_layer_emit(struct kbd_ctx* ctx, uint8_t orig_keycode, uint8_t sta
 
 	// Only alpha keys participate in SYM layers
     if (orig_keycode < KEY_A || orig_keycode > KEY_Z)
-        return 0
+        return 0;
 
 	// Third layer: SYM + SHIFT
 	if (g_sticky_altgr.held && g_phys_shift_held) {
